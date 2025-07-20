90f3dc2366a05c25596f812efba9c3a8efebd9f4
olesyamaniuk <72089671+olesyamaniuk@users.noreply.github.com>
2025-07-20 16:35:25 +0300

    2

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,8 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
   const menuBtnRef = document.querySelector("[data-menu-button]");
-  const navListRef = document.querySelector(".t-n-navigation-list");
-  const headerRef = document.querySelector(".t-n-header");
-  const navigationRef = document.querySelector(".t-n-navigation");
+  const navListRef = document.querySelector(".t-m-navigation-list");
+  const headerRef = document.querySelector(".t-m-header");
+  const navigationRef = document.querySelector(".t-m-navigation");
 
   menuBtnRef.addEventListener("click", () => {
     menuBtnRef.classList.toggle("is-open");
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", () => {
     navigationRef.classList.toggle("is-open");
   });
 
-  document.querySelectorAll(".t-n-navigation-link").forEach((n) =>
+  document.querySelectorAll(".t-m-navigation-link").forEach((n) =>
     n.addEventListener("click", () => {
       navListRef.classList.remove("is-open");
       menuBtnRef.classList.remove("is-open");
@@ -23,7 +23,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
 function toggleAnswer(element) {
   const answer = element.nextElementSibling;
-  const buttonImg = element.querySelector(".t-n-toggle-button img");
+  const buttonImg = element.querySelector(".t-m-toggle-button img");
 
   answer.classList.toggle("open");
 
--- js/slick.js
+++ js/slick.js
@@ -1,1 +1,28 @@
-
+$(function () {
+  $(".t-m-uniqueness-list").slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    variableWidth: true,
+    autoplay: true,
+    autoplaySpeed: 3500,
+    speed: 1000,
+    cssEase: "linear",
+    prevArrow: ".t-m-uniqueness-list-left",
+    nextArrow: ".t-m-uniqueness-list-right",
+  });
+});
+$(function () {
+  $(".t-m-reviews-list").slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    variableWidth: true,
+    autoplay: true,
+    autoplaySpeed: 3500,
+    speed: 1000,
+    cssEase: "linear",
+    prevArrow: ".t-m-reviews-list-left",
+    nextArrow: ".t-m-reviews-list-right",
+  });
+});
